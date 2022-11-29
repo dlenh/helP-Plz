d5beb1757dab701e0bea1d9f7082c05171c5331a
David Lenh <dlenh425@gmail.com>
2022-11-29 13:15:50 -0800

    Require dotenv file from config folder

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
-const session = require("session");
+const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
+
+require("dotenv").config({path: "./config/env"})
