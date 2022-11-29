443cec93f62cf1d24f17f3d2d9b1d86b32633418
David Lenh <dlenh425@gmail.com>
2022-11-29 14:06:02 -0800

    Edited code to work with older version of MongoStore

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 
 // use .evn file in config folder
-require("dotenv").config({path: "./config/env"})
+require("dotenv").config({ path: "./config/.env" });
 
 // passport config
 require("./config/passport")(passport)
@@ -40,7 +40,9 @@ app.use(
         secret: "keyboard cat",
         resave: false,
         saveUninitialized: false,
-        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+        store: MongoStore.create({
+            mongoUrl: process.env.DB_STRING,
+        }),
     })
 );
 
