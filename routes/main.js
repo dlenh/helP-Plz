c191acd6c57303aff29cf8d8b7bf9f0361d1246c
David Lenh <dlenh425@gmail.com>
2022-11-29 16:42:42 -0800

    Commented out code to test

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
-const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
+// const homeController = require("../controllers/home");
+// const postsController = require("../controllers/posts");
 const { ensureAuth } = require("../middleware/auth");
 
 //Main Routes 
