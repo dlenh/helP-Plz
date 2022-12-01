3d10b623847e205c4957155315f1d0bbb20c2b19
David Lenh <dlenh425@gmail.com>
2022-12-01 13:14:22 -0800

    Updated server to include comment route

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ app.use(flash());
 // set up routes for which the server is listening
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
+app.use("/comment", commentsRoutes);
 
 // server running
 app.listen(process.env.PORT, () => {
