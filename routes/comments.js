4517d57bf3301e6a59acf6e75e6860462bed586b
David Lenh <dlenh425@gmail.com>
2022-12-08 11:55:46 -0800

    Added delete comment method to router

--- routes/comments.js
+++ routes/comments.js
@@ -6,4 +6,6 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Post Routes - simplified for now
 router.post("/createComment/:id", commentsController.createComment);
 
+router.delete("/deleteComment/:id", commentsController.deleteComment);
+
 module.exports = router;
