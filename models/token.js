0fcc2a2a479683cde1e44e7eaf94fff983b67a8a
David Lenh <dlenh425@gmail.com>
2023-03-24 12:02:16 -0700

    Edited token model for email verification

--- models/token.js
+++ models/token.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
 
-const tokenSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: "user",
-        unique: true.valueOf,
-    },
+// const tokenSchema = new Schema({
+//     userId: {
+//         type: Schema.Types.ObjectId,
+//         required: true,
+//         ref: "user",
+//         unique: true.valueOf,
+//     },
+//     token: { type: String, required: true },
+//     createdAt: { type: Date, default: Date.now(), expires: 3600 }
+// })
+
+const tokenSchema = new mongoose.Schema({
+    _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
     token: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now(), expires: 3600 }
-})
+    expireAt: { type: Date, default: Date.now, index: { expires: 3600 }}
+});
 
 module.exports = mongoose.model("token", tokenSchema);
